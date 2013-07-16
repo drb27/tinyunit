520c93735ecb11f506131f2ac33da88fca7022cb
David Bradshaw <david.bradshaw.usa@gmail.com>
2013-07-16 18:53:15 -0400

    Added testcase basics
    Did some stuff
    
    Fixed incorrect attribute name in testmethod
    
    Added a git ignore file
    
    Added a comment at the head of the file describing the purpose of the TestCase class

---
+++ core/decorators.py
@@ -0,0 +1,4 @@
+
+def testmethod(f):
+    f.__testmethod__=True
+    return f
---
+++ core/testcase.py
@@ -0,0 +1,41 @@
+"""
+class: TestCase
+
+This class provides a base for all test cases in your project. 
+Deriving your test case class from TestCase informs tinyunit that your class holds
+unit tests. The engine will then find methods decorated with @testmethod, and run
+those tests automatically. 
+
+In addition, the class provides base implementations for many assert methods. 
+
+"""
+from .decorators import testmethod
+
+class TestCase(object):
+
+    class AssertFailureException(Exception):
+        def __init__(self):
+                pass
+    
+    def __init__(self):
+        pass
+
+    def assertTrue(self,expr):
+        if not expr:
+            raise TestCase.AssertFailureException()
+
+    def assertEquals(self,a,b):
+        if not a==b:
+            raise TestCase.AssertFailureException()
+
+    def assertRaises(self,fn,exception):
+        try:
+            fn()
+        except exception as e:
+            pass
+        else:
+            raise TestCase.AssertFailureException()
+
+    @testmethod
+    def dummytest(self):
+        pass
