8d35cafad7ec84bbd6ec517ba4ecb515864bbbd7
David Bradshaw <david.bradshaw.usa@gmail.com>
2013-07-17 17:11:27 -0400

    Added basic formatters

---
+++ engine/formatters.py
@@ -0,0 +1,26 @@
+class Formatter(object):
+
+        def __init__(self):
+                return ""
+
+        def format_start_case(self,case):
+                return ""
+
+        def format_end_case(self,case):
+                return ""
+
+        def format_start_set(self):
+                return ""
+
+        def format_end_set(self,cases):
+                return ""
+
+        def format_method_result(self,case,record):
+                return ""
+
+class DefaultFormatter(Formatter):
+
+        def format_method_result(self,case,record):
+                return ""
+
+
