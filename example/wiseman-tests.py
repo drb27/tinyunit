ae2cfcf8ccf410a71157a966dcb5abe102fe587a
David Bradshaw <david.bradshaw.usa@gmail.com>
2013-07-23 21:07:42 -0400

    Added XML command line switch option to example

--- example/wiseman-tests.py
+++ example/wiseman-tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from tinyunit.client import *
+import argparse
 
+from tinyunit.client import *
 from wiseman import meaning_of_life
 
 class WiseTests(TestCase):
@@ -10,5 +11,14 @@ class WiseTests(TestCase):
                 self.assertEquals(42,meaning_of_life())                
 
 if __name__=='__main__':
+
+        # Check to see if XML output is enabled
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-x","--xml",help="Output XML report", action="store_true")
+        args = parser.parse_args()
+
+        # Create an inspector to retrieve test cases automatically
         i=Inspector()
-        runset(i.cases())
+        
+        # Run a set of test cases
+        runset(i.cases(), enable_xml=args.xml)
